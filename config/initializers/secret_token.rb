d5bc81f80e670893878c83344fc7854ef24282a5
Kane Baccigalupi <baccigalupi@gmail.com>
2012-07-26 11:42:10 -0700

    basic rails app with a little js gemfile override

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WheelerDealer::Application.config.secret_token = '070c3938cd30e762932e7334456f2fcbb1c1202fdbf1c59c3ea254493ec22db59f550fe82a62ec6dd2b33d3715d41ef2d2c86f438f8b82d24a36d411684363b2'
