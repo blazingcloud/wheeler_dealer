b6c3eae3669cd254564c0dc3b47fbd770e131f1e
Kane Baccigalupi <baccigalupi@gmail.com>
2012-07-26 17:07:42 -0700

    Discard pile view, just for rendering; tapping on a card moves it to the discard pile

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,12 +2,14 @@
 //
 //= require ./card
 //= require ./deck
+//= require ./discard_pile
 
 $(document).ready(function() {
   // We don't need a full app object with all the extras for this app ...
   // so we are going to fake it out. The main thing we need is the templates
   window.app = {
-    templates: Wheel.Templates.build()
+    templates: Wheel.Templates.build(),
+    eventManager: (Modernizr.touch ? Wheel.TouchManager : Wheel.MouseManager).build()
   };
   window.app.templates.gather(); // get them from the DOM
   // now the templates are available to all the views
@@ -16,4 +18,6 @@ $(document).ready(function() {
   // That means that it will automatically append the view to that dom element. Yay!
   var deck = Deck.build({parent: $(document.body)});
   // Also inside Deck, we are building a bunch of Card views and append them to the deck view
+
+  var discard = DiscardPile.build({parent: $(document.body)});
 });
--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,6 +1,6 @@
 Wheel.View.subclass('Card', {
   // #optionize in the method that converts a passed in arguments hash into
-  // instance variables. We are overriding it here to add the suitEntity to the 
+  // instance variables. We are overriding it here to add the suitEntity to the
   // view.
   //
   // The view uses itself as the view model until a model object is provided.
@@ -9,6 +9,23 @@ Wheel.View.subclass('Card', {
   optionize: function(opts) {
     this._super(opts);
     this.suitEntity = this._class.suitMap[this.suit];
+  },
+
+  // this is where you can ad interactions
+  listen: function() {
+    this.$.on('tap', this.onTap.bind(this));
+  },
+
+  onTap: function(e) {
+    e.stopPropagation();
+    this.$.off('tap'); // stop listening for taps
+
+    this._class.zIndex = this._class.zIndex || 0;
+    this._class.zIndex ++;
+
+    this.$
+      .css('z-index', this._class.zIndex)
+      .css('left', '220px');
   }
 }, {
   suitMap: {
