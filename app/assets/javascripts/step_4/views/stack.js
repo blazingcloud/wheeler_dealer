3f5077ee431c64fe2b149e4aefd2f266f428dd5c
Kane Baccigalupi <baccigalupi@gmail.com>
2012-08-07 23:21:33 -0700

    stack view gets its positioning more better from the DOM

--- app/assets/javascripts/step_4/views/stack.js
+++ app/assets/javascripts/step_4/views/stack.js
@@ -1,9 +1,18 @@
 Wheel.View.subclass('Step4.Views.Stack', {
   init: function() {
+    var offset = this.$.offset();
+    var gameOffset = this._class.gameOffset();
+
     this.model.position({
-      left:   this.model.left ||   parseInt(this.$.css('left')) || 0,
-      top:    this.model.top ||    parseInt(this.$.css('top')) || 0,
-      zIndex: this.model.zIndex || parseInt(this.$.css('z-index')) || 0
+      left:   this.model.left ||   offset.left - gameOffset.left,
+      top:    this.model.top ||    offset.top -  gameOffset.top,
+      zIndex: this.model.zIndex || parseInt(this.$.css('z-index'))
     });
+  }
+}, {
+  gameOffset: function() {
+    if (this._gameOffset) { return this._gameOffset; }
+    this._gameOffset = $('#game .stacks').offset();
+    return this._gameOffset;
   }
 });
